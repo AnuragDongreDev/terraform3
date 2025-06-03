a1f24a9347d0e4107f5e2e35362330f2159c1807
Anurag <anurag.dongre.jobs@gmail.com>
2025-06-03 18:52:42 +0530

    Removed unrequired files

--- outputs.tf
+++
@@ -1,5 +0,0 @@
-# Dashboard outputs
-output "dashboard_url" {
-  description = "URL of the deployed Grafana dashboard"
-  value       = "${var.grafana_url}/d/${jsondecode(resource.grafana_dashboard.main.config_json).uid}"
-}
